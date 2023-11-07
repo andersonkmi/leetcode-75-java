ad27b59f15f906c41362e8a1dbba2ea61c69c082
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-11-08 00:11:34 +0100

    Add empty exercise

---
+++ src/main/java/org/codecraftlabs/leetcode75/MergeStringsAlternately.java
@@ -0,0 +1,10 @@
+package org.codecraftlabs.leetcode75;
+
+import javax.annotation.Nonnull;
+
+public class MergeStringsAlternately {
+    @Nonnull
+    public String merge(@Nonnull String word1, @Nonnull String word2) {
+        return "";
+    }
+}
---
+++ src/test/java/org/codecraftlabs/leetcode75/MergeStringsAlternatelyTest.java
@@ -0,0 +1,4 @@
+package org.codecraftlabs.leetcode75;
+
+public class MergeStringsAlternatelyTest {
+}
