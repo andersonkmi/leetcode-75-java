d64c70d01641e9f66b080a0b1ea42e6c50d68228
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-11-11 10:44:14 +0100

    Add one more unit test

--- src/main/java/org/codecraftlabs/leetcode75/ReverseVowels.java
+++ src/main/java/org/codecraftlabs/leetcode75/ReverseVowels.java
@@ -1,10 +1,47 @@
 package org.codecraftlabs.leetcode75;
 
 import javax.annotation.Nonnull;
+import java.util.Arrays;
+import java.util.Vector;
 
 public class ReverseVowels {
+    private static final String VOWELS = "aeiouAEIOU";
     @Nonnull
     public String execute(@Nonnull String word) {
-        return "";
+        int[] positions = identifyVowelPositions(word);
+        return applySwap(word, positions);
+    }
+
+    private String applySwap(@Nonnull String word, int[] originalPositions) {
+        char[] staging = word.toCharArray();
+        int middle = originalPositions.length / 2;
+        for (int index = 0; index < middle; index++) {
+            int opposite = originalPositions.length - index + 1;
+            char currentChar = staging[index];
+            char otherOne = staging[opposite];
+            staging[index] = otherOne;
+            staging[opposite] = currentChar;
+        }
+        return new String(staging);
+    }
+
+    private int[] identifyVowelPositions(@Nonnull String word) {
+        Vector<Integer> vowelsIndexes = new Vector<>();
+        for (int index = 0; index < word.length(); index++) {
+            if (isVowel(word.charAt(index))) {
+                vowelsIndexes.add(index);
+            }
+        }
+        int size = vowelsIndexes.size();
+        int[] result = new int[size];
+        for (int index = 0; index < size; index++) {
+            result[index] = vowelsIndexes.get(index);
+        }
+
+        return result;
+    }
+
+    private boolean isVowel(char item) {
+        return VOWELS.indexOf(item) != -1;
     }
 }
--- src/test/java/org/codecraftlabs/leetcode75/ReverseVowelsTest.java
+++ src/test/java/org/codecraftlabs/leetcode75/ReverseVowelsTest.java
@@ -1,8 +1,9 @@
 package org.codecraftlabs.leetcode75;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReverseVowelsTest {
     private ReverseVowels reverseVowels;
@@ -14,6 +15,12 @@ public class ReverseVowelsTest {
 
     @Test
     void when_passing_empty_string_should_return_empty() {
-        Assertions.assertThat(reverseVowels.execute("")).isBlank();
+        assertThat(reverseVowels.execute("")).isBlank();
+    }
+
+    @Test
+    void when_passing_only_one_vowel_should_remain_as_is() {
+        String word = "red";
+        assertThat(reverseVowels.execute(word)).isEqualTo(word);
     }
 }
