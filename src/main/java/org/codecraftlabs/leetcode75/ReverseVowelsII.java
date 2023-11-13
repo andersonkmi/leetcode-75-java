c27e0a05362c67f3f30e7162b59f04ed015159cc
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-11-13 21:53:23 +0100

    Initial changes

---
+++ src/main/java/org/codecraftlabs/leetcode75/ReverseVowelsII.java
@@ -0,0 +1,12 @@
+package org.codecraftlabs.leetcode75;
+
+import javax.annotation.Nonnull;
+import java.util.Set;
+
+public class ReverseVowelsII {
+    private static final Set<Character> VOWELS = Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+    @Nonnull
+    public String execute(@Nonnull String word) {
+        return "";
+    }
+}
